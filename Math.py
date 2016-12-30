5e5ed41b000fe715fd65573c0ab2843ab3094303
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2016-12-30 21:45:35 +0100

    repetitive code

--- Math.py
+++ Math.py
@@ -4,8 +4,7 @@ def getLength(vector):
     return math.sqrt(vector.x * vector.x + vector.y * vector.y)
 
 def getNormalized(vector):
-    d = math.sqrt((vector.x * vector.x) + (vector.y * vector.y))
-    return vector * (1.0 / d)
+    return vector / getLength(vector)
 
 def dotProduct(vector1, vector2):
     return vector1.x * vector2.x + vector1.y * vector2.y
