857d8afa278e674dd101340249744b7a6b3bda2f
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2017-01-01 13:21:10 +0100

    Better parameter naming

--- lib/Math.py
+++ lib/Math.py
@@ -1,13 +1,13 @@
 import math
 
-def getLength(vector):
-    return math.sqrt(vector.x * vector.x + vector.y * vector.y)
+def getLength(v):
+    return math.sqrt(v.x * v.x + v.y * v.y)
 
 def getDistance(v1, v2):
     return abs(getLength(v1-v2))
 
-def getNormalized(vector):
-    return vector / getLength(vector)
+def getNormalized(v):
+    return v / getLength(v)
 
-def dotProduct(vector1, vector2):
-    return vector1.x * vector2.x + vector1.y * vector2.y
+def dotProduct(v1, v2):
+    return v1.x * v2.x + v1.y * v2.y
